a6d5245441df038f3cef07b87e0e8cb547a2b0e2
nikkonbd <nikkon1998@gmail.com>
2023-03-28 10:51:52 +0600

    cart added

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -5,19 +5,23 @@ const Cart = (props) => {
     // console.log(props);
     const { cart } = props;
 
-    let total = 0;
+    let totalPrice = 0;
+    let totalShipping = 0;
     for (const product of cart) {
-        total = total + product.price;
+        totalPrice = totalPrice + product.price;
+        totalShipping = totalShipping + product.shipping;
     }
+    const tax = totalPrice * 7 / 100;
+    const grandTotal = totalPrice + totalShipping + tax;
 
     return (
         <div className='cart'>
             <h2>Order Summary</h2>
             <p>Selected Carts: {cart.length}</p>
-            <p>Total Price: {total}</p>
-            <p>Total Shipping Charge: </p>
-            <p>Tax: </p>
-            <h5>Grand Total: </h5>
+            <p>Total Price: ${totalPrice}</p>
+            <p>Total Shipping Charge: ${totalShipping}</p>
+            <p>Tax: ${tax.toFixed(2)}</p>
+            <h5>Grand Total: ${grandTotal.toFixed(2)} </h5>
         </div>
     );
 };
