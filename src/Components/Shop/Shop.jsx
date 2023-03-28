b1a8f0a8ed296eded12efdda783879db7a8aa3a3
nikkonbd <nikkon1998@gmail.com>
2023-03-28 13:56:27 +0600

    cart state set

--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css'
@@ -13,10 +14,29 @@ const Shop = () => {
             .then(data => setProducts(data))
     }, [])
 
+    useEffect(() => {
+        const storedCart = getShoppingCart();
+        const saveCart = [];
+        // Step 1 Get Id
+        for (const id in storedCart) {
+            // Step 2 Get the product using id
+            const addedProduct = products.find(product => product.id === id);
+            // Step 3 Get quantity of the product
+            if (addedProduct) {
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                saveCart.push(addedProduct);
+            }
+            // console.log(addedProduct);
+        }
+        setCart(saveCart);
+    }, [products])
+
     const handleClickToCart = (product) => {
         // console.log(product);
         const newCart = [...cart, product];
         setCart(newCart);
+        addToDb(product.id);
     }
 
     return (
